e9a18f651669739a563c17922282376c27f0817b
shimy-1111 <yuu1.shimi.ebi@gmail.com>
2019-11-11 14:48:39 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,20 @@
 class GroupsController < ApplicationController
-
+  before_action :set_group, only: [:edit, :update]
+  
   def index
   end
 
   def new
     @group = Group.new
     @group.users << current_user
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   def create
@@ -21,4 +30,7 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
   end
+
+  def set_group
+    @group = Group.find(params[:id])
 end
